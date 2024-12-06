15f377f5273dec09694ceaebf9e6b3b901824adc
paul-pries <paulpries.de@gmail.com>
2024-12-06 16:15:49 +0100

    Add files via upload

---
+++ Tausch.py
@@ -0,0 +1,9 @@
+#Schreibe ein Programm mit dem Namen Tausch, das den Inhalt von zwei Variablen miteinander vertauscht. Eine Variable a wird mit dem Wert 5 initialisiert und eine Variable b mit dem Wert 3. Vertausch anschließend die Werte beider Variablen und gib die Werte aus. Dabei muss bei a der Wert 3 und bei b der Wert 5 ausgegeben werden.
+
+a = 5
+b = 3
+
+a, b = b, a
+
+print(a)
+print(b)
