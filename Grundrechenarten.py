0d71f9e2af943c794de8aa9214d6db6e5cd258ad
paul-pries <paulpries.de@gmail.com>
2024-12-07 15:42:44 +0100

    Add files via upload

---
+++ Grundrechenarten.py
@@ -0,0 +1,13 @@
+# 
+
+x = 10 # Variable x wird definiert
+y = 9 # Variable y wird definiert
+a = (x+y) # Addition von x und y
+b = (x-y) # Subtraktion von x und y
+c = (x*y) # Multiplikation von x und y
+d = (x/y) # Division von x und y
+
+print(a) # Gib die Variable a in der Konsole aus
+print(b) # Gib die Variable b in der Konsole aus
+print(c) # Gib die Variable c in der Konsole aus
+print(d) # Gib die Variable d in der Konsole aus
