abea331fc20dd5c1bb56286336281849064dfa18
paul-pries <paulpries.de@gmail.com>
2024-12-10 22:03:32 +0100

    Add files via upload

---
+++ Dreieck.py
@@ -0,0 +1,8 @@
+#Schreibe ein Programm mit dem Namen Dreieck, das aus der Länge einer Grundseite und der auf dieser Seite stehenden Höhe den Flächeninhalt bestimmt. Deklariere und initialisiere jeweils eine Variable für alle drei Werte und gib sie anschließend schön formatiert aus, d.h. mit Antwortsatz.
+
+grundseite = 4
+hoehe = 2
+
+a = (grundseite*hoehe)/2
+
+print("Der Flächeninhalt beträgt ", a, "!")
