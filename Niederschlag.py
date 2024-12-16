d8ae95bacc522ea6425a0f6a84f0f3503b335cf7
paul-pries <paulpries.de@gmail.com>
2024-12-16 17:13:22 +0100

    Add files via upload

---
+++ Niederschlag.py
@@ -0,0 +1,27 @@
+# Schreibe ein Programm mit dem Namen Niederschlag, das die durchschnittliche Niederschlagsmenge für die Monate April, Mai und Juni berechnet. Deklariere und initialisiere jeweils eine Variable für jeden Monat. Berechne anschließend die durchschnittliche Niederschlagsmenge sowohl in einem Rechenschritt(mit einer Variable z.B. average) als auch in zwei Rechenschritten und gib die Ergebnisse in folgender Form aus:
+
+# Niederschlag im April : 12
+
+# Niederschlag im Mai : 14
+
+# Niederschlag im Juni : 8
+
+# Durchschnitt 1 Schritt : …
+
+# Durchschnitt 2 Schritte: …
+
+April = 12
+Mai = 14
+Juni = 8
+Gesamt_Niederschlag = (April+Mai+Juni)
+
+average = (April+Mai+Juni)/3
+
+zwei_Schritt1 = (April+Mai+Juni)
+zwei_Schritt2 = (zwei_Schritt1)/3
+
+print("Niederschlag im April:", April)
+print("Niederschlag im Mai:", Mai)
+print("Niederschlag im Juni:", Juni)
+print("Durchschnitt 1 Schritt:", average)
+print("Durchschnitt 2 Schritte",zwei_Schritt2)
