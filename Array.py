2660de67bbc70c51d436bf69e1c735dbb3fc69e5
paul-pries <paulpries.de@gmail.com>
2025-02-06 16:56:06 +0100

    Add files via upload

---
+++ Array.py
@@ -0,0 +1,11 @@
+# Schreibe ein Programm, das eine Liste von fünf Zahlen enthält und das größte Element daraus findet.
+
+zahlen = [3, 7, 1, 9, 4]
+
+# Größte Zahl finden
+
+groesste_zahl = max(zahlen)
+
+# Ergebnis ausgeben
+
+print(f"Die größte Zahl ist: {groesste_zahl}")
